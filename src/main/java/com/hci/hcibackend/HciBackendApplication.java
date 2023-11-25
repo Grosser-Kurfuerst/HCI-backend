a84c377bc2e3847ef6985882a8c5004890210d6f
Großer Kurfürst <bai3641@outlook.com>
2023-11-25 22:40:15 +0800

    init spring

---
+++ src/main/java/com/hci/hcibackend/HciBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hci.hcibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HciBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HciBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hci/hcibackend/HciBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hci.hcibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HciBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
