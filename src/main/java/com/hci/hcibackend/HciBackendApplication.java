e3fdaf42e55b952297c4cc8214efb2003985d493
Großer Kurfürst <bai3641@outlook.com>
2023-12-07 21:52:10 +0800

    实现请求拦截器检查jwt

--- src/main/java/com/hci/hcibackend/HciBackendApplication.java
+++ src/main/java/com/hci/hcibackend/HciBackendApplication.java
@@ -1,13 +1,28 @@
 package com.hci.hcibackend;
 
+import com.hci.hcibackend.utils.jwt.JwtAuthenticationFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @MapperScan("com.hci.hcibackend.mapper")
 @SpringBootApplication
-public class HciBackendApplication {
-
+public class HciBackendApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HciBackendApplication.class);
+    }
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter();
+        registrationBean.setFilter(filter);
+        return registrationBean;
+    }
     public static void main(String[] args) {
         SpringApplication.run(HciBackendApplication.class, args);
     }
