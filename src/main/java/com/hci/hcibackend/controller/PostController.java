0f1c05e0d646ec17162338f477a03c322910583b
86150 <2041733900@qq.com>
2023-12-24 14:30:31 +0800

    feat：post update and delete

--- src/main/java/com/hci/hcibackend/controller/PostController.java
+++ src/main/java/com/hci/hcibackend/controller/PostController.java
@@ -9,7 +9,11 @@ import com.hci.hcibackend.model.vo.PostVO;
 import com.hci.hcibackend.service.BmsPostService;
 import com.hci.hcibackend.service.UmsUserService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
+import com.vdurmont.emoji.EmojiParser;
+import java.util.Date;
 
 import java.util.Map;
 import java.util.List;
@@ -51,6 +55,26 @@ public class PostController extends BaseController {
     public ApiResult<List<BmsPost>> getRecommend(@RequestParam("topicId") String id) {
         List<BmsPost> topics = bmsPostService.getRecommend(id);
         return ApiResult.success(topics);
+    }
+
+    @PostMapping("/update")
+    public ApiResult<BmsPost> update(@RequestHeader(value = USER_NAME) String userName, @Valid @RequestBody BmsPost post) {
+        UmsUser umsUser = umsUserService.getUserByUsername(userName);
+        Assert.isTrue(umsUser.getId().equals(post.getUserId()), "非本人无权修改");
+        post.setModifyTime(new Date());
+        post.setContent(EmojiParser.parseToAliases(post.getContent()));
+        bmsPostService.updateById(post);
+        return ApiResult.success(post);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ApiResult<String> delete(@RequestHeader(value = USER_NAME) String userName, @PathVariable("id") String id) {
+        UmsUser umsUser = umsUserService.getUserByUsername(userName);
+        BmsPost byId = bmsPostService.getById(id);
+        Assert.notNull(byId, "来晚一步，话题已不存在");
+        Assert.isTrue(byId.getUserId().equals(umsUser.getId()), "你为什么可以删除别人的话题？？？");
+        bmsPostService.removeById(id);
+        return ApiResult.success(null,"删除成功");
     }
 
 }
