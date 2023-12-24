847fac3304ceabc0b5c8fb1e7b6528e58ac08435
GuaziPolaris <91023123+GuaziPolaris@users.noreply.github.com>
2023-12-24 15:39:00 +0800

    添加评论

--- src/main/java/com/hci/hcibackend/service/impl/BmsCommentServiceImpl.java
+++ src/main/java/com/hci/hcibackend/service/impl/BmsCommentServiceImpl.java
@@ -2,13 +2,16 @@ package com.hci.hcibackend.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hci.hcibackend.mapper.BmsCommentMapper;
+import com.hci.hcibackend.model.dto.CommentDTO;
 import com.hci.hcibackend.model.entity.BmsComment;
+import com.hci.hcibackend.model.entity.UmsUser;
 import com.hci.hcibackend.model.vo.CommentVO;
 import com.hci.hcibackend.service.BmsCommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
@@ -24,5 +27,17 @@ public class BmsCommentServiceImpl extends ServiceImpl<BmsCommentMapper, BmsComm
             log.info("lstBmsComment失败");
         }
         return lstBmsComment;
+    }
+
+    @Override
+    public BmsComment create(CommentDTO dto, UmsUser user) {
+        BmsComment comment = BmsComment.builder()
+                .userId(user.getId())
+                .content(dto.getContent())
+                .topicId(dto.getTopic_id())
+                .createTime(new Date())
+                .build();
+        this.baseMapper.insert(comment);
+        return comment;
     }
 }
