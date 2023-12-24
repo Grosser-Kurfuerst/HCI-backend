7ff6c56f96653f7636763a9b46e333a71f1d7d37
86150 <2041733900@qq.com>
2023-12-24 13:59:28 +0800

    feat:add follow

---
+++ src/main/java/com/hci/hcibackend/controller/RelationshipController.java
@@ -0,0 +1,85 @@
+package com.hci.hcibackend.controller;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.hci.hcibackend.common.ApiResult;
+import com.hci.hcibackend.common.exception.ApiAsserts;
+import com.hci.hcibackend.model.entity.BmsFollow;
+import com.hci.hcibackend.model.entity.UmsUser;
+import com.hci.hcibackend.service.BmsFollowService;
+import com.hci.hcibackend.service.UmsUserService;
+import jakarta.annotation.Resource;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.hci.hcibackend.utils.jwt.JwtUtil.USER_NAME;
+
+@RestController
+@RequestMapping("/relationship")
+public class RelationshipController extends BaseController {
+
+    @Resource
+    private BmsFollowService bmsFollowService;
+
+    @Resource
+    private UmsUserService umsUserService;
+
+    @GetMapping("/subscribe/{userId}")
+    public ApiResult<Object> handleFollow(@RequestHeader(value = USER_NAME) String userName
+            , @PathVariable("userId") String parentId) {
+        UmsUser umsUser = umsUserService.getUserByUsername(userName);
+        if (parentId.equals(umsUser.getId())) {
+            ApiAsserts.fail("不可以关注自己哦 😮");
+        }
+        BmsFollow one = bmsFollowService.getOne(
+                new LambdaQueryWrapper<BmsFollow>()
+                        .eq(BmsFollow::getParentId, parentId)
+                        .eq(BmsFollow::getFollowerId, umsUser.getId()));
+        if (!ObjectUtils.isEmpty(one)) {
+            ApiAsserts.fail("已关注");
+        }
+
+        BmsFollow follow = new BmsFollow();
+        follow.setParentId(parentId);
+        follow.setFollowerId(umsUser.getId());
+        bmsFollowService.save(follow);
+        return ApiResult.success(null, "关注成功");
+    }
+
+    @GetMapping("/unsubscribe/{userId}")
+    public ApiResult<Object> handleUnFollow(@RequestHeader(value = USER_NAME) String userName
+            , @PathVariable("userId") String parentId) {
+        UmsUser umsUser = umsUserService.getUserByUsername(userName);
+        BmsFollow one = bmsFollowService.getOne(
+                new LambdaQueryWrapper<BmsFollow>()
+                        .eq(BmsFollow::getParentId, parentId)
+                        .eq(BmsFollow::getFollowerId, umsUser.getId()));
+        if (ObjectUtils.isEmpty(one)) {
+            ApiAsserts.fail("未关注！");
+        }
+        bmsFollowService.remove(new LambdaQueryWrapper<BmsFollow>().eq(BmsFollow::getParentId, parentId)
+                .eq(BmsFollow::getFollowerId, umsUser.getId()));
+        return ApiResult.success(null, "取关成功");
+    }
+
+    @GetMapping("/validate/{topicUserId}")
+    public ApiResult<Map<String, Object>> isFollow(@RequestHeader(value = USER_NAME) String userName
+            , @PathVariable("topicUserId") String topicUserId) {
+        UmsUser umsUser = umsUserService.getUserByUsername(userName);
+        Map<String, Object> map = new HashMap<>(16);
+        map.put("hasFollow", false);
+        if (!ObjectUtils.isEmpty(umsUser)) {
+            BmsFollow one = bmsFollowService.getOne(new LambdaQueryWrapper<BmsFollow>()
+                    .eq(BmsFollow::getParentId, topicUserId)
+                    .eq(BmsFollow::getFollowerId, umsUser.getId()));
+            if (!ObjectUtils.isEmpty(one)) {
+                map.put("hasFollow", true);
+            }
+        }
+        return ApiResult.success(map);
+    }
+}
